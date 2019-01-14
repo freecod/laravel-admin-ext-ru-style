273ad5b19cf76fcd1ef05c58b8b678781452c773
vagrant <vagrant@homestead>
2019-01-14 07:56:48 +0000

    init

---
+++ src/RuStyle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace freecod\LaravelAdminExt\RuStyle;
+
+use Encore\Admin\Extension;
+
+class RuStyle extends Extension
+{
+    public $name = 'ru-style';
+    public $assets = __DIR__.'/../resources/assets';
+}
---
+++ src/RuStyleServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace freecod\LaravelAdminExt\RuStyle;
+
+use Encore\Admin\Admin;
+use Illuminate\Support\ServiceProvider;
+
+class RuStyleServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot(RuStyle $extension)
+    {
+        if (! RuStyle::boot()) {
+            return ;
+        }
+
+        if ($this->app->runningInConsole() && $assets = $extension->assets()) {
+            $this->publishes(
+                [$assets => public_path('vendor/laravel-admin-ext/ru-style')],
+                'ru-style'
+            );
+        }
+    }
+    
+    public function handle()
+    {
+	    Admin::booting(function () {
+	    	
+		    $baseCss = Admin::baseCss();
+		    $key = array_search('vendor/laravel-admin/google-fonts/fonts.css', $baseCss);
+		    if ($key !== false) {
+			    $baseCss[$key] = 'vendor/laravel-admin-ext/ru-style/fonts/source-sans-pro/source-sans-pro.css';
+			    Admin::baseCss($baseCss);
+		    }
+		
+		    Admin::css('vendor/laravel-admin-ext/ru-style/css/laravel-admin.css');
+	    });
+    }
+}
